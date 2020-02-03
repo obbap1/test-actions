fa0a44f3fb06d53b192f459561f1fdc9276e9aae
Paschal Obba <paschalobba@gmail.com>
2020-02-03 18:26:21 +0100

    welp

--- .github/scripts/scp.sh
+++ .github/scripts/scp.sh
@@ -4,8 +4,6 @@
 
 set -eu
 
-echo $source $username $host $target
-
-scp -i $HOME/secrets/pbaba.pem $source $username@$host$target
+scp -i $HOME/secrets/pbaba.pem $source $username@$host:$target
 
 echo "I am done baby!"
