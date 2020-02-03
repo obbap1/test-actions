9acec2a83c2b3eb0710ca9d57891f44fe87e6800
Paschal Obba <paschalobba@gmail.com>
2020-02-03 17:12:30 +0100

    fix: fix bash script

--- .github/scripts/decrypt.sh
+++ .github/scripts/decrypt.sh
@@ -2,4 +2,5 @@
 
 mkdir $HOME/secrets
 
-gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" --output $HOME/secrets/secret.pem ${ENC_FILE:- secret.pem.gpg}
+gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
+--output $HOME/secrets/secret.pem ${ENC_FILE:- pbaba.pem.gpg}
