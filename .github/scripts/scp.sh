b726b05e09c3bfee23d785f8fcd57d85faf58479
Paschal Obba <paschalobba@gmail.com>
2020-02-27 13:43:46 +0100

    fix: try action

--- .github/scripts/scp.sh
+++ .github/scripts/scp.sh
@@ -4,6 +4,12 @@
 
 set -eu
 
+sudo chmod 600 ~/secrets/pbaba.pem
+
+sudo chmod 644 ~/.ssh/known_hosts
+
+sudo chmod 755 ~/.ssh
+
 scp -o StrictHostKeyChecking=no -v -P $port -i $HOME/secrets/pbaba.pem $source $username@$host:$target
 
 echo "I am done baby!!!!"
